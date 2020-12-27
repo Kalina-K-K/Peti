2065e3172aa6c31ab5c696db56fecf09b944be06
Kalina-K-K <72562377+Kalina-K-K@users.noreply.github.com>
2020-12-27 21:39:32 +0200

    Add files via upload

---
+++ ComicBook.java
@@ -0,0 +1,21 @@
+package edu5;
+
+public class ComicBook extends Book {
+	private String characterName;
+	public ComicBook(double regularPrice, String publisher, int yearPublished, String characterName) {
+		super(regularPrice, publisher, yearPublished);
+		this.setCharacterName(characterName);
+	}
+	
+	public double computeSalePrice(){
+        return super.getRegularPrice() * 0.4;
+    }
+
+	public String getCharacterName() {
+		return characterName;
+	}
+
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+}
---
+++ Product.java
@@ -0,0 +1,22 @@
+package edu5;
+
+public abstract class Product {
+	    
+	    private double regularPrice;
+	    
+	    /** Creates a new instance of Product */
+	    public Product(double regularPrice) {
+	        this.regularPrice = regularPrice;
+	    }
+	    
+	    // Abstract method
+	    public abstract double computeSalePrice();
+
+	    public double getRegularPrice() {
+	        return regularPrice;
+	    }
+
+	    public void setRegularPrice(double regularPrice) {
+	        this.regularPrice = regularPrice;
+	    }
+}
